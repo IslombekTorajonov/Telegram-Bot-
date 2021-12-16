4217178c4e6bdca91586ace94a6259045ea4161a
developer <90696696+IslombekTorajonov@users.noreply.github.com>
2021-12-16 16:30:51 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,23 +5,27 @@ from config import bot_token
 
 bot = Bot(bot_token)
 dp = Dispatcher(bot)
+
+
 @dp.message_handler(Command("start"))
 @dp.message_handler(Command("help"))
-
 @dp.message_handler(Command("kanal"))
-
 @dp.message_handler()
 async def lalala(message: Message):
     if message.text == "/start":
-       await bot.send_message(chat_id=message.from_user.id, text="Salom hurmatli foydalanuvchi bizning botimizga xush kelibsiz iltimos savolingizni yollang biz sizga tez orada javob beramiz")
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Salom hurmatli foydalanuvchi bizning botimizga xush kelibsiz iltimos savolingizni yollang biz sizga tez orada javob beramiz")
 
     if message.text == "/kanal":
-       await bot.send_message(chat_id=message.from_user.id, text="Hurmatli foydalanuvchi bizning rasmiy kanal bot biogrammasida korsatib qoyilgan bemalol kirib ozingiz uchun kerakli malumotga ega bolishingiz mumkin")
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Hurmatli foydalanuvchi bizning rasmiy kanal bot biogrammasida korsatib qoyilgan bemalol kirib ozingiz uchun kerakli malumotga ega bolishingiz mumkin")
 
     if message.text == "/help":
-        await bot.send_message(chat_id=message.from_user.id,text="Hurmatli foydalanuvchi biz sizga qanday yordam korsata olamiz bizga savol yoki takliflaringizni yollang bit sizga tez orada javob beramiz")
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Hurmatli foydalanuvchi biz sizga qanday yordam korsata olamiz bizga savol yoki takliflaringizni yollang biz sizga tez orada javob beramiz")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     from aiogram import executor
+
     executor.start_polling(dp)
